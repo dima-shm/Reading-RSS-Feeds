a6770d501e09ee5f4e437987348eb86ecf72e991
Dima Shumanski <dima.shumanski@gmail.com>
2019-02-07 19:03:22 +0300

    Add models

---
+++ RSSFeedsReader/WebApp/Models/Channel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp.Models
+{
+    class Channel
+    {
+        [Key]
+        public string Link { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Language { get; set; }
+    }
+}
