e0727010178d57beed281fe805e3e1a8f4eafe66
Dima Shumanski <dima.shumanski@gmail.com>
2019-02-07 17:48:08 +0300

    Added Models

---
+++ RSSFeedsReader/ConsoleApp/Models/Article.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsoleApp.Models
+{
+    class Article
+    {
+        [Key, Column(Order = 1)]
+        public string Title { get; set; }
+
+        public string Link { get; set; }
+
+        public string Description { get; set; }
+
+        [Key, Column(Order = 2)]
+        public string PubDate { get; set; }
+
+        public string ChannelId { get; set; }
+        [ForeignKey("ChannelId")]
+        private Channel Channel { get; set; }
+
+        public Article() { }
+
+        public Article(string title, string link, string description, string pubDate, string channelId)
+        {
+            Title = title;
+            Link = link;
+            Description = description;
+            PubDate = pubDate;
+            ChannelId = channelId;
+        }      
+    }
+}
