20983a9e21cb42014b974b8d2797a9cab3a0474c
Dima Shumanski <dima.shumanski@gmail.com>
2019-02-07 17:50:02 +0300

    Create ApplicationContext.cs

---
+++ RSSFeedsReader/ConsoleApp/Context/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp.Models;
+using System.Data.Entity;
+
+namespace ConsoleApp.Context
+{
+    class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("RSSReaderDB") { }
+
+        public DbSet<Channel> Channels { get; set; }
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
