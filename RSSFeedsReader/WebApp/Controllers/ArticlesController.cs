f0783104cc5fcabfc8c3d9fcfc2273b0738a68bc
Dima Shumanski <dima.shumanski@gmail.com>
2019-02-08 13:18:19 +0300

    Update ArticlesController.cs
    Refactor action

--- RSSFeedsReader/WebApp/Controllers/ArticlesController.cs
+++ RSSFeedsReader/WebApp/Controllers/ArticlesController.cs
@@ -12,22 +12,36 @@ namespace WebApp.Controllers
 
         public IHttpActionResult Get([FromUri]string selectedChannel, [FromUri]int selectedSort)
         {
+            List<Article> articles;
+            articles = getArticlesFromChannel(context.Articles.ToList(), selectedChannel);
+            articles = getArticlesWithSort(articles, selectedSort);
+            return Ok(articles);
+        }
+
+        private List<Article> getArticlesFromChannel(List<Article> articles, string channelName)
+        {
+            List<Article> articlesInChannel = articles;
             Channel channel;
-            List<Article> articlesInChannel;
-            if (selectedChannel != "All")
+            if (channelName != "All")
             {
-                channel = context.Channels.First(c => c.Description == selectedChannel);
+                channel = context.Channels.First(c => c.Description == channelName);
                 articlesInChannel = context.Articles.Where(a => a.ChannelId == channel.Link).ToList();
             }
             else
             {
                 articlesInChannel = context.Articles.ToList();
             }
-            if (selectedSort == 1)
-                articlesInChannel = articlesInChannel.OrderByDescending(a => a.PubDate).ToList();
-            else if (selectedSort == 2)
-                articlesInChannel = articlesInChannel.OrderByDescending(a => a.ChannelId).ToList();
-            return Ok(articlesInChannel);
+            return articlesInChannel;
+        }
+
+        private List<Article> getArticlesWithSort(List<Article> articles, int sortMode)
+        {
+            List<Article> sortedArticles = articles;
+            if (sortMode == 1)
+                sortedArticles = articles.OrderByDescending(a => a.PubDate).ToList();
+            else if (sortMode == 2)
+                sortedArticles = articles.OrderByDescending(a => a.ChannelId).ToList();
+            return sortedArticles;
         }
     }
 }
